80c690970a314f0f3e9bceeaad187c18fc2782e1
Sam Demeulemeester <samdemeulemeester@hotmail.com>
2022-07-31 21:51:49 +0200

    Create JPA config class

--- src/main/java/com/samdml/common/JpaConfig.java
+++ src/main/java/com/samdml/common/JpaConfig.java
@@ -1,2 +1,14 @@
-package com.samdml.common;public class JpaConfig {
+package com.samdml.common;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JpaConfig {
+
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
+
+    public static EntityManager getEntityManager() {
+        return entityManagerFactory.createEntityManager();
+    }
 }
