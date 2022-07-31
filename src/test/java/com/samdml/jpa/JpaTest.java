03b20688caae34eff7f3e24f2a36ae7003d4ca3b
Sam Demeulemeester <samdemeulemeester@hotmail.com>
2022-07-31 21:55:24 +0200

    Create jpa test

--- src/test/java/com/samdml/jpa/JpaTest.java
+++ src/test/java/com/samdml/jpa/JpaTest.java
@@ -1,2 +1,18 @@
-package com.samdml.jpa;public class JpaTest {
+package com.samdml.jpa;
+
+import com.samdml.common.JpaConfig;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+
+public class JpaTest {
+
+    @Test
+    public void test() {
+        EntityManager entityManager = JpaConfig.getEntityManager();
+        entityManager.getTransaction().begin();
+
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
 }
